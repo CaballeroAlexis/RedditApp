6e64e7cb34c02479bb1a572d6d8e206ed9345be2
Alexis <alexis.wolfy@gmail.com>
2018-10-22 20:34:33 -0300

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,61 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.Keyboard) {
-      window.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+  var app=angular.module('starter', ['ionic'])
+  app.controller('RedditCtrl',function($scope,$http){
+    $scope.posts=[];
+    $http.get("https://www.reddit.com/r/gaming/new/.json")
+    .success(function(posts){
+      angular.forEach(posts.data.children,function(post) {
+        $scope.posts.push(post.data);
+        
+      });
+      console.log(posts);
+    });
+    $scope.doInfinite=function(){
+      var params={};
+      if($scope.posts.length  > 0){
+        params['after']=$scope.posts[$scope.posts.length  -1].name;
 
-    if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
-      StatusBar.styleDefault();
-    }
+      }
+      $http.get("https://www.reddit.com/r/gaming/new/.json",{params:params})
+      .success(function(posts){
+        angular.forEach(posts.data.children,function(post) {
+          $scope.posts.push(post.data);
+        });
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    })
+  };
+  $scope.doRefresh=function(){
+    var params={'before':$scope.posts[0].name};
+    $http.get("https://www.reddit.com/r/gaming/new/.json",{params:params})
+    .success(function(posts){
+      var newPost=[];
+      angular.forEach(posts.data.children,function(post) {
+        newPost.push(post.data);
+      });
+      $scope.posts=newPost.concat($scope.posts);
+      $scope.$broadcast('scroll.refreshComplete');
+  })    
+  }
+
+  $scope.openLink=function(url){
+    window.open(url,"_blank");
+  }
+}
+);
+
+  app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+
+      if (window.cordova && window.Keyboard) {
+        window.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.cordova && window.cordova.InAppBrowser){
+        window.open = window.cordova.InAppBrowser.open;
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   });
-})
